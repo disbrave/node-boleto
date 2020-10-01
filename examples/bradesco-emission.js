7854fed59254f9add7b24705e74cfe3f61d79782
Vitor Costa <vitorrcosta96@gmail.com>
2020-10-01 11:50:43 -0300

    [NEW] Adicao de tipo de boleto (cosntruir codigo na hora vs. informar na criacao

--- examples/bradesco-emission.js
+++ examples/bradesco-emission.js
@@ -4,7 +4,7 @@ var path = require('path')
 var app = express()
 
 var Boleto = require('../index').Boleto
-
+61
 var boleto = new Boleto({
   'banco': 'bradesco',
   'data_emissao': new Date(),
@@ -19,17 +19,18 @@ var boleto = new Boleto({
   'carteira': '25',
   'pagador': 'Nome do pagador\nCPF: 000.000.000-00',
   'local_de_pagamento': 'PAGÁVEL EM QUALQUER BANCO ATÉ O VENCIMENTO.',
-  'instrucoes': 'Sr. Caixa, aceitar o pagamento e não cobrar juros após o vencimento.'
-})
-
-// console.log(boleto['linha_digitavel']);
+  'instrucoes': 'Sr. Caixa, aceitar o pagamento e não cobrar juros após o vencimento.',
+  'codigo_barras': '23793839500000098613416090605614099300018240',
+  'numero_documento':'23793416019060561409993000182407383950000009861',
+},2)
 
 app.use(express.static(path.join(__dirname, '/../')))
 
 app.get('/', function (req, res) {
-  boleto.renderHTML(function (html) {
-    return res.send(html)
-  })
+  // boleto.renderHTML(function (html) {
+  //   return res.send(html)
+  // })
+  res.send(boleto.returnRenderedHTML())
 })
 
 app.listen(3003)
